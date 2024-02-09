13133e2cdbf8738de34964f91aa7420815558b98
Hubjerd <hubi.hey@vp.pl>
2024-02-09 21:38:33 +0100

    First steps towards face stabilization, added PID class

--- Dron_tello/facialDetection.py
+++ Dron_tello/facialDetection.py
@@ -1,12 +1,85 @@
 from djitellopy import tello
 import cv2
+import mediapipe as mp
+import tkinter as tk
+import pygame as pg
+
+class PID:
+    def __init__(self,p,i,d):
+        self.kp = p
+        self.ki = i
+        self.kd = d
+        self.previous_error = 0
+        self.integral = 0
+    def update(self,error,delta_time):
+        # Standard PID - integrate, derive, lastly the output will determine how much the drone should move
+        self.integral += error * delta_time
+        derivative = (error-self.previous_error) / delta_time
+        # Here we need to establish how much the drone should move away       
+        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        
+        self.previous_error = error
+        return output
 
 me = tello.Tello()
 me.connect()
-
 me.streamon()
+cam = me.get_frame_read()
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection()
+
+pid = PID(0.1,0.01,0.1)
+
+window = tk.Tk()
+window.tilte("Control Panel")
+finish = False
+move_amount = 10
+airborne = False
+actions = {pg.K_W:me.move_forward,pg.K_D:me.move_right,pg.K_A:me.move_left,pg.K_S:me.move_back}
+desired_width = 200
+desired_height = 300
 
 while True:
-    img = me.get_frame_read().frame
-    cv2.imshow('huj' , img)
-    cv2.waitKey(1)
+    events = pg.event.get()
+    for event in events:
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_R and not airborne:
+                me.takeoff()
+                airborne = not airborne
+            elif event.key == pg.K_R:
+                me.land()
+                airborne = not airborne
+    keys = pg.key.get_pressed()
+    for key in actions:
+        if keys[key]:
+            actions[key](move_amount)
+    if finish:
+        break
+    image = cam.frame
+    cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image)
+    if results.detections:
+        for detection in results.detections:
+            mp_drawing.draw_detection(detection)
+
+            if airborne:
+                # Here the position correction happens
+                height = detection.location_data.relative_bounding_box.height
+                width = detection.location_data.relative_bounding_box.width
+                relative_width = image.shape[1] * width
+                relative_height = image.shape[0] * height
+                
+                # PID control
+                error_width = desired_width - relative_width
+                error_height = desired_height - relative_height
+                move_factor = pid.update
+            
+            print(detection)
+    cv2.imshow("Faces",image)
+    cv2.waitKey(0)
+
+
+me.land()
+cv2.destroyAllWindows()
