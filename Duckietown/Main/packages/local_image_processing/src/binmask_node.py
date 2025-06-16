f38e2c793d025ce4d29067c0a288d6992c3fdf8f
firiusz123 <firiusz123@gmail.com>
2025-06-16 13:56:14 +0200

    added the histogram matching , still need some fixes + need to extablish the ideal image colors distribution

--- Duckietown/Main/packages/local_image_processing/src/binmask_node.py
+++ Duckietown/Main/packages/local_image_processing/src/binmask_node.py
@@ -31,6 +31,8 @@ class Binmask(DTROS):
         # Search area of followed line
         self.searchArea = DTParam('~search_area', param_type=ParamType.DICT)
 
+        #need to get rid of this asap
+        self.default_target_image = cv2.imread("/home/duckie/Documents/LFRobotics/Duckietown/Main/packages/local_image_processing/config/binmask_node/frame_0.png")
         # Convert color mask to np.array
         self.colorLineMask = {k : np.array(v) for k, v in self.color.value.items()}
         
@@ -55,10 +57,56 @@ class Binmask(DTROS):
         # Transformed image
         self.pubDebugImage = rospy.Publisher('~image/out/compressed', CompressedImage, queue_size=1)
 
+
+    def match_histograms(source, target_bgr):
+        # Convert BGR to LAB
+        source_lab = cv2.cvtColor(source, cv2.COLOR_BGR2LAB)
+        target_lab = cv2.cvtColor(target_bgr, cv2.COLOR_BGR2LAB)
+
+        matched_lab = np.zeros_like(source_lab)
+
+        # For each channel in LAB
+        for ch in range(3):
+            src = source_lab[:, :, ch].ravel()
+            tgt = target_lab[:, :, ch].ravel()
+
+            # For A and B channels (ch=1,2), shift values to [0,255]
+            if ch != 0:
+                src = src.astype(np.int16) - 128  # Assuming LAB values from OpenCV: A and B in [0,255]
+                tgt = tgt.astype(np.int16) - 128
+
+            # Compute histograms (256 bins over the valid range)
+            src_hist, bins = np.histogram(src, bins=256, range=(0,255), density=True)
+            tgt_hist, _ = np.histogram(tgt, bins=256, range=(0,255), density=True)
+
+            src_cdf = np.cumsum(src_hist)
+            tgt_cdf = np.cumsum(tgt_hist)
+
+            src_cdf = src_cdf / src_cdf[-1]
+            tgt_cdf = tgt_cdf / tgt_cdf[-1]
+
+            lookup = np.zeros(256, dtype=np.uint8)
+            for i in range(256):
+                diff = np.abs(tgt_cdf - src_cdf[i])
+                lookup[i] = np.argmin(diff)
+
+            # Map source pixels
+            if ch == 0:
+                matched_lab[:, :, ch] = lookup[source_lab[:, :, ch]]
+            else:
+                matched_lab[:, :, ch] = lookup[source_lab[:, :, ch]]
+
+        # Convert back LAB to BGR
+        matched_bgr = cv2.cvtColor(matched_lab, cv2.COLOR_LAB2BGR)
+        return matched_bgr
+
     def callback(self, msg) -> None:
         try:           
             # Read input image            
             image = self.cvbridge.compressed_imgmsg_to_cv2(msg)
+            #applying the histogram matching 
+            image = self.match_histograms(image ,self.default_target_image )
+            
             image = cv2.blur(image,(5,5))
             
             # Convert image to HSV color space
